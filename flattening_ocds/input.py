c96f66b7badd70fa7032801593dc59a9c7198e77
Ben Webb <bjwebb67@googlemail.com>
2014-11-04 18:31:52 +0000

    Restructure input code to seperate out CSV specific code

--- flattening_ocds/input.py
+++ flattening_ocds/input.py
@@ -2,9 +2,44 @@ from csv import DictReader
 import os
 import json
 
-def un_flatten_line(line):
+
+class SpreadsheetInput(object):
+    def __init__(self, input_name='', main_sheet_name=''):
+        self.input_name = input_name
+        self.main_sheet_name = main_sheet_name
+        self.sub_sheets_names = []
+
+    def get_main_sheet_lines(self):
+        return self.get_sheet_lines(self.main_sheet_name)
+
+    def get_sub_sheets_lines(self):
+        for sub_sheet_name in self.sub_sheets_names:
+            yield sub_sheet_name, self.get_sheet_lines(sub_sheet_name)
+
+    def get_sheet_lines(self, sheet_name):
+        raise NotImplementedError
+
+
+class CSVInput(SpreadsheetInput):
+    def read_sheets(self):
+        sheet_file_names = os.listdir(self.input_name)
+        if self.main_sheet_name+'.csv' not in sheet_file_names:
+            raise ValueError
+        sheet_file_names.remove(self.main_sheet_name+'.csv')
+
+        if not all([fname.endswith('.csv') for fname in sheet_file_names]):
+            raise ValueError
+        self.sub_sheet_names = [fname[:-4] for fname in sheet_file_names]
+
+    def get_sheet_lines(self, sheet_name):
+        with open(os.path.join(self.input_name, sheet_name+'.csv')) as main_sheet_file:
+            for line in DictReader(main_sheet_file):
+                yield line
+
+
+def unflatten_line(line):
     unflattened = {}
-    for k,v in line.items():
+    for k, v in line.items():
         if v == '':
             continue
         fields = k.split('.')
@@ -17,29 +52,21 @@ def un_flatten_line(line):
     return unflattened
 
 
-def read_csv_dir(folder, main_sheet_name='release'):
-    sheets = os.listdir(folder)
-    if not main_sheet_name+'.csv' in sheets:
-        raise ValueError
-
-    sheets.remove(main_sheet_name+'.csv')
-
+def unflatten(spreadsheet_input):
     main_sheet_by_ocid = {}
-    with open(os.path.join(folder, main_sheet_name+'.csv')) as main_sheet_file:
-        for line in DictReader(main_sheet_file):
-            if line['ocid'] in main_sheet_by_ocid:
-                raise ValueError('Two lines in main spreadsheet with same ocid')
-            main_sheet_by_ocid[line['ocid']] = un_flatten_line(line)
+    for line in spreadsheet_input.get_main_sheet_lines():
+        if line['ocid'] in main_sheet_by_ocid:
+            raise ValueError('Two lines in main spreadsheet with same ocid')
+        main_sheet_by_ocid[line['ocid']] = unflatten_line(line)
 
-    for sheet_file_name in sheets:
-        with open(os.path.join(folder, sheet_file_name)) as sheet_file:
-            if not sheet_file_name.endswith('.csv'):
-                raise ValueError
-            sheet_name = sheet_file_name[:-4]
-            for line in DictReader(sheet_file):
-                main_sheet_by_ocid[line['ocid']][sheet_name] = un_flatten_line(line)
+    for sheet_name, lines in spreadsheet_input.get_sub_sheets_lines():
+        for line in lines:
+            main_sheet_by_ocid[line['ocid']][sheet_name] = unflatten_line(line)
 
-    print(json.dumps(list(main_sheet_by_ocid.values()), indent=2))
+    return main_sheet_by_ocid.values()
+
 
 if __name__ == '__main__':
-    read_csv_dir('release_input')
+    spreadsheet_input = CSVInput(input_name='release_input', main_sheet_name='release')
+    spreadsheet_input.read_sheets()
+    print(json.dumps(list(unflatten(spreadsheet_input)), indent=2))
--- flattening_ocds/tests/test_input.py
+++ flattening_ocds/tests/test_input.py
@@ -1,4 +1,9 @@
-from flattening_ocds.input import read_csv_dir
+from flattening_ocds.input import unflatten_line
 
-def test_read_csv_dir():
-    read_csv_dir('release_input')
+
+def test_unflatten_line():
+    # Check flat fields remain flat
+    assert unflatten_line({'a': 1, 'b': 2}) == {'a': 1, 'b': 2}
+    assert unflatten_line({'a.b': 1, 'a.c': 2, 'd.e': 3}) == {'a': {'b': 1, 'c': 2}, 'd': {'e': 3}}
+    # Check more than two levels of nesting, and that multicharacter fields aren't broken
+    assert unflatten_line({'fieldA.b.c.d': 'value'}) == {'fieldA':{'b':{'c':{'d':'value'}}}}
