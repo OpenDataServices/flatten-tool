8255449613cb721ece23b822b8ef380a31f9b0bc
Ben Webb <bjwebb67@googlemail.com>
2014-11-07 15:51:14 +0000

    Add some unit tests of the unflatten function

--- flattening_ocds/tests/test_input.py
+++ flattening_ocds/tests/test_input.py
@@ -1,6 +1,19 @@
 from flattening_ocds.input import unflatten_line, SpreadsheetInput, unflatten
 
 
+class ListInput(SpreadsheetInput):
+    def __init__(self, sheets, **kwargs):
+        self.sheets = sheets
+        super(ListInput, self).__init__(**kwargs)
+
+    def get_sheet_lines(self, sheet_name):
+        print(sheet_name)
+        return self.sheets[sheet_name]
+
+    def read_sheets(self):
+        self.sub_sheet_names = list(self.sheets.keys()).remove(self.main_sheet_name)
+
+
 def test_unflatten_line():
     # Check flat fields remain flat
     assert unflatten_line({'a': 1, 'b': 2}) == {'a': 1, 'b': 2}
@@ -8,3 +21,39 @@ def test_unflatten_line():
     # Check more than two levels of nesting, and that multicharacter fields aren't broken
     assert unflatten_line({'fieldA/b/c/d': 'value'}) == {'fieldA': {'b': {'c': {'d': 'value'}}}}
 
+
+class TestUnflatten(object):
+    def test_main_sheet_flat(self):
+        spreadsheet_input = ListInput(
+            sheets={
+                'custom_main': [
+                    {
+                        'ocid': 1,
+                        'id': 2,
+                        'testA': 3,
+                    }
+                ]
+            },
+            main_sheet_name='custom_main')
+        spreadsheet_input.read_sheets()
+        assert list(unflatten(spreadsheet_input)) == [
+            {'ocid': 1, 'id': 2, 'testA': 3}
+        ]
+
+    def test_main_sheet_nonflat(self):
+        spreadsheet_input = ListInput(
+            sheets={
+                'custom_main': [
+                    {
+                        'ocid': 1,
+                        'id': 2,
+                        'testA/testB': 3,
+                        'testA/testC': 4,
+                    }
+                ]
+            },
+            main_sheet_name='custom_main')
+        spreadsheet_input.read_sheets()
+        assert list(unflatten(spreadsheet_input)) == [
+            {'ocid': 1, 'id': 2, 'testA': {'testB': 3, 'testC': 4}}
+        ]
