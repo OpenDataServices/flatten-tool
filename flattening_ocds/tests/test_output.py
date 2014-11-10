aa6ab6f2e30374f4ef63ce01e77d2b6e90b6a3b6
Ben Webb <bjwebb67@googlemail.com>
2014-11-10 12:50:14 +0000

    [#24] Ensure tests always write into tmpdir

--- flattening_ocds/tests/test_output.py
+++ flattening_ocds/tests/test_output.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from flattening_ocds import output, schema
 
 
@@ -17,21 +18,23 @@ def test_spreadsheetouput_base_fails():
         spreadsheet_output.write_sheets()
 
 
-def test_blank_sheets():
+def test_blank_sheets(tmpdir):
     for spreadsheet_output_class in output.FORMATS.values():
         spreadsheet_output = spreadsheet_output_class(
             parser=MockParser([], {}),
-            main_sheet_name='release')
+            main_sheet_name='release',
+            output_name=os.path.join(tmpdir.strpath, 'release'))
         spreadsheet_output.write_sheets()
     # TODO Actually check the sheets are blank
 
 
-def test_populated_sheets():
+def test_populated_sheets(tmpdir):
     for spreadsheet_output_class in output.FORMATS.values():
         subsheet = schema.SubSheet()
         subsheet.add_field('c')
         spreadsheet_output = spreadsheet_output_class(
             parser=MockParser(['a'], {'b': subsheet}),
-            main_sheet_name='release')
+            main_sheet_name='release',
+            output_name=os.path.join(tmpdir.strpath, 'release'))
         spreadsheet_output.write_sheets()
     # TODO Actually check the sheets are populated
