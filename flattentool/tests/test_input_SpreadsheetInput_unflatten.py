5d4c1bcd970120142a6f1e351ace7f2076d1f11b
Ben Webb <ben.webb@opendataservices.coop>
2016-02-11 12:21:15 +0000

    Add tests of convert_titles with an empty schema

--- flattentool/tests/test_input_SpreadsheetInput_unflatten.py
+++ flattentool/tests/test_input_SpreadsheetInput_unflatten.py
@@ -6,6 +6,7 @@ This file only covers tests for the main sheet. Tests for multiple sheets are in
 """
 from __future__ import unicode_literals
 from .test_input_SpreadsheetInput import ListInput
+from flattentool.schema import SchemaParser
 from decimal import Decimal
 from collections import OrderedDict
 import sys
@@ -26,6 +27,9 @@ def inject_root_id(root_id, d):
     return d
 
 UNICODE_TEST_STRING = 'éαГ😼𝒞人'
+
+# TODO Actually create appropriate schema
+SCHEMA = {}
 
 # ROOT_ID will be replace by the appropirate root_id name in the test (e.g. ocid)
 testdata = [
@@ -106,6 +110,8 @@ testdata = [
     )
 ]
 
+# Convert titles modes: with appropirate schema, without, off
+@pytest.mark.parametrize('convert_titles,use_schema', [(False, False), (True, False), (True, True)])
 @pytest.mark.parametrize('root_id,root_id_kwargs',
     [
         ('ocid', {}), # If not root_id kwarg is passed, then a root_id of ocid is assumed
@@ -114,7 +120,9 @@ testdata = [
         ('', {'root_id': ''})
     ])
 @pytest.mark.parametrize('input_list,expected_output_list', testdata)
-def test_unflatten(root_id, root_id_kwargs, input_list, expected_output_list, recwarn):
+def test_unflatten(convert_titles, use_schema, root_id, root_id_kwargs, input_list, expected_output_list, recwarn):
+    extra_kwargs = {'convert_titles': convert_titles}
+    extra_kwargs.update(root_id_kwargs)
     spreadsheet_input = ListInput(
         sheets={
             'custom_main': [
@@ -122,8 +130,15 @@ def test_unflatten(root_id, root_id_kwargs, input_list, expected_output_list, re
             ]
         },
         main_sheet_name='custom_main',
-        **root_id_kwargs)
+        **extra_kwargs)
     spreadsheet_input.read_sheets()
+    if convert_titles:
+        parser = SchemaParser(
+            root_schema_dict=SCHEMA if use_schema else {},
+            use_titles=True
+        )
+        parser.parse()
+        spreadsheet_input.parser = parser
     expected_output_list = [
         inject_root_id(root_id, expected_output_dict) for expected_output_dict in expected_output_list
     ]
@@ -132,5 +147,6 @@ def test_unflatten(root_id, root_id_kwargs, input_list, expected_output_list, re
         expected_output_list = []
     assert list(spreadsheet_input.unflatten()) == expected_output_list
     # We expect no warnings
-    assert recwarn.list == []
+    if not convert_titles: # TODO what are the warnings here
+        assert recwarn.list == []
 
