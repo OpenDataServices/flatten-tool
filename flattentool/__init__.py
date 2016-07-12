61c604577f96782f91360f752b9f3828fb05559a
James Gardner <james@3aims.com>
2016-07-12 12:50:59 +0100

    Support for decimal in Python 3.5.2

--- flattentool/__init__.py
+++ flattentool/__init__.py
@@ -79,24 +79,12 @@ def flatten(input_name, schema=None, output_name='flattened', output_format='all
         raise Exception('The requested format is not available')
 
 
-# From http://bugs.python.org/issue16535
-class NumberStr(float):
-    def __init__(self, o):
-        # We don't call the parent here, since we're deliberately altering it's functionality
-        # pylint: disable=W0231
-        self.o = o
-
-    def __repr__(self):
-        return str(self.o)
-
-    # This is needed for this trick to work in python 3.4
-    def __float__(self):
-        return self
-
-
 def decimal_default(o):
     if isinstance(o, Decimal):
-        return NumberStr(o)
+        if int(o) == o:
+            return int(o)
+        else:
+            return float(o)
     raise TypeError(repr(o) + " is not JSON serializable")
 
 
