d8cac7a4d632e87900d649a9503e3ab5f6c006fd
Ben Webb <ben.webb@opendataservices.coop>
2017-08-21 14:26:15 +0100

    Multiplesheets flatten tests

--- flattentool/tests/test_json_input_is_unflatten_reversed.py
+++ flattentool/tests/test_json_input_is_unflatten_reversed.py
@@ -4,8 +4,10 @@ testdata in reverse.
 '''
 
 from .test_input_SpreadsheetInput_unflatten import ROOT_ID_PARAMS, testdata, testdata_titles, create_schema, inject_root_id
+from .test_input_SpreadsheetInput_unflatten_mulitplesheets import testdata_multiplesheets
 from flattentool.json_input import JSONParser
 from flattentool.schema import SchemaParser
+from collections import OrderedDict
 import pytest
 import json
 
@@ -71,3 +73,48 @@ def test_flatten_titles(root_id, root_id_kwargs, input_list, expected_output_lis
         pytest.skip()
     return test_flatten(use_titles=True, use_schema=True, root_id=root_id, root_id_kwargs=root_id_kwargs, input_list=input_list, expected_output_list=expected_output_list, recwarn=recwarn, comment=comment, warning_messages=warning_messages, reversible=reversible, tmpdir=tmpdir)
 
+
+
+# Don't test with use_titles and use_schema true because this will will use
+# titles, which the fixtures don't
+@pytest.mark.parametrize('use_titles,use_schema', [(False, False), (True, False), (False, True)])
+@pytest.mark.parametrize('root_id,root_id_kwargs', ROOT_ID_PARAMS)
+@pytest.mark.parametrize('comment,expected_output_dict,input_list,warning_messages,reversible', testdata_multiplesheets)
+def test_flatten_multiplesheets(use_titles, use_schema, root_id, root_id_kwargs, input_list, expected_output_dict, recwarn, comment, warning_messages, tmpdir, reversible):
+    if not reversible:
+        pytest.skip()
+
+    # Not sure why, but this seems to be necessary to have warnings picked up
+    # on Python 2.7 and 3.3, but 3.4 and 3.5 are fine without it
+    import warnings
+    warnings.simplefilter('always')
+
+    extra_kwargs = {'use_titles': use_titles}
+    extra_kwargs.update(root_id_kwargs)
+    
+    if use_schema:
+        schema_parser = SchemaParser(
+            root_schema_dict=create_schema(root_id) if use_schema else {"properties": {}},
+            rollup=True,
+            **extra_kwargs
+        )
+        schema_parser.parse()
+    else:
+        schema_parser = None
+
+    with tmpdir.join('input.json').open('w') as fp:
+        json.dump({
+            'mykey': [inject_root_id(root_id, input_row) for input_row in input_list]
+        }, fp)
+
+    parser = JSONParser(
+        json_filename=tmpdir.join('input.json').strpath,
+        root_list_path='mykey',
+        schema_parser=schema_parser,
+        **extra_kwargs)
+    parser.parse()
+
+    expected_output_dict = OrderedDict([(sheet_name, [inject_root_id(root_id, line) for line in lines]) for sheet_name, lines in expected_output_dict.items()])
+    output = {sheet_name:sheet.lines for sheet_name, sheet in parser.sub_sheets.items() if sheet.lines}
+    output['custom_main'] = parser.main_sheet.lines
+    assert output == expected_output_dict
