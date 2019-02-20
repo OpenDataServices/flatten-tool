d572c1aa70e01211f3c08b6fa51e3d0345c5f308
James Baster <james.baster@opendataservices.coop>
2019-02-20 15:19:39 +0000

    Lock openpyxl to 2.5 only - not 2.6 https://github.com/OpenDataServices/flatten-tool/issues/287

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import sys
 
-install_requires = ['jsonref', 'schema', 'openpyxl>=2.5', 'six', 'pytz', 'xmltodict', 'lxml']
+install_requires = ['jsonref', 'schema', 'openpyxl>=2.5,<2.6', 'six', 'pytz', 'xmltodict', 'lxml']
 
 if sys.version < '3':
     install_requires.append('unicodecsv')
