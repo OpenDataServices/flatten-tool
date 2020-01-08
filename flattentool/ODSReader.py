4cbef24479f2e69d8e5720d947c36c1a13b1a14b
Jared Parnell <jared.parnell@opendataservices.coop>
2020-01-08 15:36:23 +0000

    fix: ODS unicode support and test

--- flattentool/ODSReader.py
+++ flattentool/ODSReader.py
@@ -72,7 +72,7 @@ class ODSReader:
                         if (n.nodeType == 1 and n.tagName == "text:span"):
                             for c in n.childNodes:
                                 if (c.nodeType == 3):
-                                    textContent = u'{}{}'.format(textContent, n.data)
+                                    textContent = u'{}{}'.format(textContent, c.data)
 
                         if (n.nodeType == 3):
                             textContent = u'{}{}'.format(textContent, n.data)
