16d60f0bcbd5a32e002c59d17c4949499a67d450
Ben Webb <ben.webb@opendataservices.coop>
2020-10-09 16:42:34 +0000

    locale: Set up babel and transifex
    Babel setup includes compiling po to mo files as part of installation.
    
    https://github.com/open-contracting/cove-oc4ids/issues/59

--- setup.py
+++ setup.py
@@ -1,4 +1,31 @@
 from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+
+def run_compile_catalog(setuptools_command):
+    from babel.messages.frontend import compile_catalog
+
+    compiler = compile_catalog(setuptools_command.distribution)
+    option_dict = setuptools_command.distribution.get_option_dict("compile_catalog")
+    compiler.domain = [option_dict["domain"][1]]
+    compiler.directory = option_dict["directory"][1]
+    compiler.use_fuzzy = option_dict["use_fuzzy"][1]
+    compiler.run()
+
+
+# From https://stackoverflow.com/questions/40051076/compile-translation-files-when-calling-setup-py-install
+class InstallWithCompile(install):
+    def run(self):
+        run_compile_catalog(self)
+        super().run()
+
+
+class DevelopWithCompile(develop):
+    def run(self):
+        run_compile_catalog(self)
+        super().run()
+
 
 install_requires = [
     "jsonref",
@@ -22,4 +49,7 @@ setup(
     description="Tools for generating CSV and other flat versions of the structured data",
     install_requires=install_requires,
     extras_require={"HTTP": ["requests"]},
+    cmdclass={"install": InstallWithCompile, "develop": DevelopWithCompile,},
+    package_data={"flattentool": ["locale/*/*/*.mo", "locale/*/*/*.po"]},
+    setup_requires=["babel"],
 )
